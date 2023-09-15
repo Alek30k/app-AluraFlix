cd4c28fc245bfedc8970fafff818061db19b0c98
Alek30k <alemank23k@gmail.com>
2023-09-15 13:26:52 -0300

    last commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
-import logo from "../../assets/img/logo.png";
+import logo from "../../assets/img/LogoMain.png";
 const Navbar = () => {
   return (
     <div className="navbar">
