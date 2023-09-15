a64e0217b061e2e6de9c6a1c4e29f3fe5472a86f
Alek30k <alemank23k@gmail.com>
2023-09-14 22:25:28 -0300

    last commit

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
-  //   baseURL: "https://csarta.onrender.com/api",
+  // baseURL: "http://localhost:8800/api/",
+  baseURL: "https://appflix.onrender.com/api",
   // withCredentials: true,
 });
 
